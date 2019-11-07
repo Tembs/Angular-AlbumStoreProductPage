364deca7030a7bae3a2c6194c0ee142abe898f9c
Tembs <Tembs@users.noreply.github.com>
2019-11-07 15:54:38 +0200

    Completed module

---
+++ src/app/product.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+@Injectable()
+export class ProductService {
+	private _albunUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
+private class _albumUrl 
+}
